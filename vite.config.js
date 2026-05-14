221b8cd02d1e088fbec7e9643ff9e23da1f4fdd5
pauldurban-gif <paul.durban@gmail.com>
2026-05-14 08:45:07 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/main.jsx',
+      name: 'TurnoverCalculator',
+      fileName: () => 'turnover-calculator.js',
+      formats: ['iife'],
+    },
+  }
 })
